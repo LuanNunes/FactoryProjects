115be9e76a40e93c57c11f4752da7534a8b86b9e
luan <nunes.lfa@gmail.com>
2019-03-08 15:15:16 -0300

    Code challenge to insert 4kk (Four million of rows into database).
    Code challenge to insert 4kk (Four million of rows into database).

---
+++ CodeChallengeFourKk/CodeChallengeFourKk/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeChallengeFourKk
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public static IEnumerable<Customer> Generate(int count)
+        {
+            for (var i = 0; i < count; i++)
+            {
+                yield return new Customer
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = "FirstName " + i,
+                    LastName = "LastName " + i,
+                    Age = 10 + i,
+                    DateOfBirth = DateTime.UtcNow
+                };
+            }
+        }
+    }
+}
---
+++ CodeChallengeFourKk/CodeChallengeFourKk/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace CodeChallengeFourKk
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             CREATE TABLE [dbo].[Customer](
+              [Id] [uniqueidentifier] NOT NULL,
+              [FirstName] [nvarchar](150) NULL,
+              [LastName] [nvarchar](150) NULL,
+              [Age] [int] NULL,
+              [DateOfBirth] [datetime2](7) NULL)
+             */
+
+            var customers = Customer.Generate(4000000);
+
+
+            //TODO: Insert this in database
+        }
+    }
+}
